bb605f6031c3de08ba983d2fd3bd8a7c6570966c
HenokTsegaye <maxhenock@gmail.com>
2021-07-01 18:30:19 +0300

    feat: adding a progress bar on top

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: false,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
